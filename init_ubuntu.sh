94b2017fd1c6d02badae4000488fadea6fbdc616
h-watanb <29752757+hrtwt@users.noreply.github.com>
2024-07-13 09:29:49 +0900

    Update init_ubuntu.sh

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -9,6 +9,6 @@ apt-get install -y build-essential zip unzip
 apt-get install -y language-pack-ja
 
 update-locale LANG=ja_JP.UTF8
-timedatectl set-timezone Asia/Tokyo
+localectl set-locale LANG=ja_JP.UTF-8
 
 apt-get install -y manpages-ja manpages-ja-dev
